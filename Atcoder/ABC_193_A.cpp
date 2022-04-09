6ce811d0f7fa6317e0feed9655faf99d6117ab24
notslok <53101134+notslok@users.noreply.github.com>
2022-04-10 01:26:03 +0530

    Add files via upload

---
+++ Atcoder/ABC_193_A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define mod 1007
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+void solve()
+{
+  int a,b;
+  cin>>a>>b;
+
+  int d= a-b;
+  double c = double(d)/a;
+  //cout<<c<<endl;
+  cout<<fixed<<setprecision(2)<<c*100.0<<endl;
+}
+
+int32_t main()
+{
+  fastio;
+  int t;
+  t=1;
+  //cin>>t;
+  while(t--)
+  {
+    solve();
+  }
+  return 0;
+}
